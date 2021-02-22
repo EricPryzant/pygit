d9755c338adef141b5991b4d69fe1fea69e2dd4f
PZ <PZ@ericpryzant.com>
2021-02-22 16:21:52 -0600

    Added init to create new directory

--- pygit/cli.py
+++ pygit/cli.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from . import data
 
 def main():
     args = parse_args()
@@ -13,4 +16,5 @@ def parse_args():
     return parser.parse_args()
 
 def init(args):
-    print("Hello, I'm initialized!")
+    data.init()
+    print(f"Initialized empty pygit repository in {os.getcwd()}/{data.GIT_DIR}")
