62cb2590ce9293cf244b2193b0b37c76301eeb03
PZ <PZ@ericpryzant.com>
2021-02-23 12:31:20 -0600

    Added type protection to hash function

--- pygit/cli.py
+++ pygit/cli.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from . import data
+from . import base
 
 def main():
     args = parse_args()
@@ -36,4 +37,4 @@ def hash_object(args):
 
 def cat_file(args):
     sys.stdout.flush()
-    sys.stdout.buffer.write(data.get_object(args.object))
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
