d5d69ebca8f6781462819f171f7abc31f579ea3a
PZ <PZ@ericpryzant.com>
2021-02-23 12:48:15 -0600

    Added function to ignore .git and .pygit folders

--- pygit/base.py
+++ pygit/base.py
@@ -6,7 +6,15 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
+
+def is_ignored(path):
+    tmp = path.split('/')
+    if '.pygit' in tmp: return True
+    if '.git' in tmp: return True
+    return False
